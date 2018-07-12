ab9de6312cc94f5b3ad6adb1a87c0712dfd98867
matejsokol89 <sokolvm@gmail.com>
2018-07-12 23:15:35 +0200

    Ljetni zadatak, prvi dio, tek jedna tablica, dolazi još.

---
+++ LjetniZadatak/src/sokol/ljetnizadatak/LjetniZadatak.java
@@ -0,0 +1,17 @@
+
+package sokol.ljetnizadatak;
+
+/**
+ *
+ * @author Sokol
+ */
+
+//START KLASA
+public class LjetniZadatak {
+
+    
+    public static void main(String[] args) {
+        new NbaBaza().setVisible(true);
+    }
+    
+}
